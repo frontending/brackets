05b4c57dc74a48b1d335e91f63b3490d070a0e3e
frontending <mymage259@gmail.com>
2021-03-11 01:00:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketArr = bracketsConfig.map(function (el) {
+    return el.join('')
+  })
+  let flag = false;
+  while (str.length !== 0) {
+    if (flag) {
+      return false
+    }
+    flag = true;
+    for (let i = 0; i < bracketArr.length; i++) {
+      if (str.includes(bracketArr[i])) {
+        flag = false
+        str = str.replace(bracketArr[i], '')
+      }
+    }
+  }
+  return true
 }
